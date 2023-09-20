85cd13a485afa8535b353780633bebd976283a79
zradacore <zradacore@gmail.com>
2023-09-20 16:12:04 +0300

    Initial Commit

---
+++ ReduxExample/ReduxExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxExampleApp.swift
+//  ReduxExample
+//
+//  Created by Владимир Никитин on 20.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ReduxExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
